6133c3926d5f7b124789f4267deb6031a1d797e1
Dawid Górecki <dgorecki@amu.edu.pl>
2022-03-22 11:13:21 +0100

    Added Game class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String [] args) {
-        
+        // Create game object and start the game
+        Game game = new Game();
+        game.start();
     }
 }

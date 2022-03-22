e8ee3c10eb50290d6e5051945b731e1c864447da
Dawid Górecki <dgorecki@amu.edu.pl>
2022-03-22 12:04:05 +0100

    Getting random title

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,39 @@
-public class Game {
-    public void start() {
+import java.io.File;
+import java.util.Scanner;
 
+public class Game {
+    String fileName;
+
+    public Game() {
+        this.fileName = "list.txt";
+    }
+
+    public Game(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void start() throws Exception {
+        int points = 10;
+        String randomTitle = getRandomMovieTitle(this.fileName);
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println(randomTitle); // TEST
+
+
+    }
+
+    private String getRandomMovieTitle(String fileName) throws Exception {
+        // Load file and create array with movies titles
+        Scanner sc = new Scanner(new File(fileName));
+        String[] movies = new String[100];
+        int n = 0;
+
+        while (sc.hasNextLine()) {
+            movies[n++] = sc.nextLine();
+        }
+
+        // Return random title
+        int randomIndex = (int) (Math.random() * n);
+        return movies[randomIndex];
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String [] args) {
         // Create game object and start the game
-        Game game = new Game();
-        game.start();
+        Game game = new Game("movies.txt");
+
+        try {
+            game.start();
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
     }
 }

32d9917c20b0fb742a4d8cc66ad750f20c86a8e6
Dawid Górecki <kontakt.dawidgorecki@gmail.com>
2022-03-23 13:59:24 +0100

    Added game logic

--- src/Game.java
+++ src/Game.java
@@ -2,29 +2,61 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Game {
-    String fileName;
+    private int points;
+    private String randomTitle;
+    private String matchedLetters;
+    private String wrongLetters;
+    private boolean guessed;
 
-    public Game() {
-        this.fileName = "list.txt";
-    }
-
-    public Game(String fileName) {
-        this.fileName = fileName;
+    public Game(String filename) throws Exception {
+        points = 10;
+        randomTitle = this.getRandomMovieTitle(filename);
+        matchedLetters = "";
+        wrongLetters = "";
+        guessed = false;
     }
 
     public void start() throws Exception {
-        int points = 10;
-        String randomTitle = getRandomMovieTitle(this.fileName);
-        Scanner inputScanner = new Scanner(System.in);
+        while (points > 0) {
+            System.out.println("You are guessing:" + getCoveredMovieTitle());
+            System.out.println("You have guessed (" + wrongLetters.length() / 2 + ") wrong letters:" + wrongLetters);
 
-        System.out.println(randomTitle); // TEST
+            System.out.print("Guess a letter: ");
+            Scanner input = new Scanner(System.in);
+            String letter = input.nextLine().toLowerCase();
 
+            if (!letter.matches("[a-z]")) {
+                System.out.println("[!] " + letter + " is not a letter.");
+                continue;
+            }
 
+            if (matchedLetters.contains(letter) || wrongLetters.contains(letter)) {
+                System.out.println("[!] You already guessed letter \"" + letter + "\".");
+                continue;
+            }
+
+            if (randomTitle.toLowerCase().contains(letter)) {
+                matchedLetters += letter;
+            } else {
+                wrongLetters += " " + letter;
+                points--;
+            }
+
+            if (!getCoveredMovieTitle().contains("_")) {
+                guessed = true;
+                break;
+            }
+        }
+
+        if (guessed) {
+            System.out.println("[!] You win! You have guessed '" + randomTitle + "' correctly.");
+        } else {
+            System.out.println("[!] You lose. Correct title was " + randomTitle + "'.");
+        }
     }
 
-    private String getRandomMovieTitle(String fileName) throws Exception {
-        // Load file and create array with movies titles
-        Scanner sc = new Scanner(new File(fileName));
+    private String getRandomMovieTitle(String filename) throws Exception {
+        Scanner sc = new Scanner(new File(filename));
         String[] movies = new String[100];
         int n = 0;
 
@@ -35,5 +67,13 @@ public class Game {
         // Return random title
         int randomIndex = (int) (Math.random() * n);
         return movies[randomIndex];
+    }
+
+    private String getCoveredMovieTitle() {
+        if (matchedLetters.isEmpty()) {
+            return randomTitle.replaceAll("[a-zA-Z]", "_");
+        }
+
+        return randomTitle.replaceAll("[^" + matchedLetters + " ]", "_");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
-    public static void main(String [] args) {
-        // Create game object and start the game
-        Game game = new Game("movies.txt");
-
+    public static void main(String[] args) {
         try {
+            Game game = new Game("movies.txt");
             game.start();
         } catch (Exception e) {
             System.out.println("Exception: " + e.getMessage());
